20a20fa55005c8ac8547d6fb34483e9ca8a327c8
manoj <manojgodhani00@gmail.com>
2018-04-11 16:39:16 +0530

    changes

--- src/actions/types.js
+++
@@ -1,1 +0,0 @@
-export const SET_COMPONENTS = 'SET_COMPONENTS';
--- src/reducers/components.js
+++
@@ -1,9 +0,0 @@
-import { SET_COMPONENTS } from '../actions/types';
-
-export default (state = [], action = {}) => {
-  switch (action.type) {
-    case SET_COMPONENTS:
-      return action.payload;
-    default: return state;
-  }
-}
